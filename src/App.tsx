89db7ab5b2c99ca2144e0cb078f6ee5b2d4b870f
richhuth <richhuth@gmail.com>
2022-12-15 15:56:58 -0500

    mastyer

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="logo">
+          type-safe : Studio <br />
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
